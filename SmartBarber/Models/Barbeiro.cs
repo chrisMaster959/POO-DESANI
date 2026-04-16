1689841b26484d212a023043f391871ec1528519
Aluno <aluno@fatecriopreto.edu.br.local>
2026-04-16 16:37:04 -0300

    Entrada dos Models

---
+++ SmartBarber/Models/Barbeiro.cs
@@ -0,0 +1,7 @@
+public class Barbeiro : Pessoa
+{
+    public string Logradouro { get; set;}
+    public int Nr_Logradouro { get; set;}
+    public string Bairro { get; set; }
+    public int cep { get; set; } 
+}
